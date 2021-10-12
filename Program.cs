0d991cdb5878723b6aaff64cc9c4422470c25afd
KyleDixonVO <KyleDixon@hotmail.ca>
2021-10-12 10:03:26 -0300

    Added ammo and weapon name arrays

--- Program.cs
+++ Program.cs
@@ -11,29 +11,49 @@ namespace Prog1_Arrays
         static void Main(string[] args)
         {
             Console.WriteLine("Ammo System:");
+            Console.WriteLine("");
+
             int weapon = 0;
 
-            Console.WriteLine("BEFORE Arrays: ");
+            //Console.WriteLine("BEFORE Arrays: ");
+            //Console.WriteLine("");
             int pistolAmmo = 6;
             int shotGunAmmo = 2;
             int bfgAmmo = 20;
 
-            if (weapon == 0)
-            {
-                Console.WriteLine("Pistol Ammo: " + pistolAmmo);
-            }
-            else if (weapon == 1)
-            {
-                Console.WriteLine("Shot Gun Ammo: " + shotGunAmmo);
-            }
-            else if (weapon == 4)
-            {
-                Console.WriteLine("Bfg Ammo" + bfgAmmo);
-            }
+            //if (weapon == 0)
+            //{
+            //    Console.WriteLine("Pistol Ammo: " + pistolAmmo);
+            //    Console.WriteLine("");
+            //}
+            //else if (weapon == 1)
+            //{
+            //    Console.WriteLine("Shot Gun Ammo: " + shotGunAmmo);
+            //    Console.WriteLine("");
+            //}
+            //else if (weapon == 2)
+            //{
+            //    Console.WriteLine("Bfg Ammo" + bfgAmmo);
+            //    Console.WriteLine("");
+            //}
 
 
             Console.WriteLine("Arrays: ");
+            Console.WriteLine("");
 
+            int[] ammo; // "type[]" declaring an array, "int[]" declares an array of integers.
+            ammo = new int[3];
+            ammo[0] = 6;
+            ammo[1] = 2;
+            ammo[2] = 20;
+
+            string[] weaponName = new string[3];
+            weaponName[0] = "Pistol";
+            weaponName[1] = "Shot Gun";
+            weaponName[2] = "BFG";
+
+
+            Console.WriteLine(weaponName[weapon] + " Ammo: " + ammo[weapon]);
 
 
 
